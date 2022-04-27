9da53c9fd43c0ecbbff7b96ce25b7cf4794f63df
ITsverkunov <tsverkunovivan@gmail.com>
2022-04-27 15:52:08 +0300

    Updates

---
+++ precache-manifest.3454b037e8dfc042cb013bc91b12fa27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c825c089de2e296aedc83f290c57c72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2899da9618c9e982c415",
+    "url": "/static/css/main.2f9319ef.chunk.css"
+  },
+  {
+    "revision": "9ff6689e4a4e75d3fd2b",
+    "url": "/static/js/2.2201ef58.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2201ef58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2899da9618c9e982c415",
+    "url": "/static/js/main.c34efd28.chunk.js"
+  },
+  {
+    "revision": "46976c2c4ede7b9bcf75",
+    "url": "/static/js/runtime-main.a5584f08.js"
+  }
+]);
